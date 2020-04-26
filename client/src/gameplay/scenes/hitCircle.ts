07ebbfaa0ac67184a666c0332749bd30b2c20610
Scott Kim <scottk3@illinois.edu>
2020-04-26 18:58:46 +0900

    Add circle timing using tweens

--- client/src/gameplay/scenes/hitCircle.ts
+++ client/src/gameplay/scenes/hitCircle.ts
@@ -10,28 +10,34 @@ export default class HitCircle {
     private timingGraphic: Phaser.GameObjects.Graphics;
     private startTime: number;
 
+    public active: boolean;
+    public alpha: number;
+    public timingRadius: number;
+
     readonly MAX_TIMING_RADIUS: number = 300;
     readonly HIT_RADIUS: number = 65;
     readonly DURATION_MS: number = 2000;
 
+    readonly BORDER_WIDTH: number = 5;
+
     constructor(scene: Phaser.Scene, x: number, y: number) {
         // this.hitSound = hitSound;
-        this.startTime = Date.now();
         this.x = x;
         this.y = y;
+
+        this.alpha = 0;
+        this.active = false;
 
         this.hitSound = scene.sound;
 
         const hitCircle = new Phaser.Geom.Circle(0, 0, this.HIT_RADIUS);
 
         this.timingGraphic = scene.add.graphics();
-
-        this.timingGraphic.lineStyle(10, 0xabcede, 1);
-        this.timingGraphic.strokeCircleShape(this.getTimingCircle(0.0));
+        this.timingGraphic.lineStyle(this.BORDER_WIDTH, 0xabcede);
+        this.timingGraphic.strokeCircleShape(this.getTimingCircle());
 
         this.hitGraphic = scene.add.graphics();
-
-        this.hitGraphic.lineStyle(10, 0xffffff, 1);
+        this.hitGraphic.lineStyle(this.BORDER_WIDTH, 0xffffff);
         this.hitGraphic.strokeCircleShape(hitCircle);
         this.hitGraphic.fillStyle(0x112288, 1);
         this.hitGraphic.fillCircleShape(hitCircle);
@@ -39,28 +45,40 @@ export default class HitCircle {
         this.hitGraphic.setInteractive(hitCircle, Phaser.Geom.Circle.Contains);
         this.hitGraphic.on('pointerdown', this.onClick.bind(this));
 
+        this.timingRadius = this.MAX_TIMING_RADIUS;
+
         this.group = scene.add.group([this.timingGraphic, this.hitGraphic]);
         this.group.setX(x);
         this.group.setY(y);
+        this.group.setAlpha(0);
+    }
+
+    start(): void {
+        this.startTime = Date.now();
+        this.active = true;
+    }
+
+    done(): void{
+        this.active = false;
     }
 
     update(): void {
-        const progress = (Date.now() - this.startTime) / this.DURATION_MS;
-        if (progress > 1) {
+        if (!this.active) {
             return;
         }
+        this.group.setAlpha(this.alpha);
+
         this.timingGraphic.clear();
-        this.timingGraphic.lineStyle(10, 0xabcede, 1);
-        this.timingGraphic.strokeCircleShape(this.getTimingCircle(progress));
+        this.timingGraphic.lineStyle(this.BORDER_WIDTH, 0xabcede);
+        this.timingGraphic.strokeCircleShape(this.getTimingCircle());
     }
 
     private onClick(): void {
         this.hitSound.play('softHitclap');
     }
 
-    private getTimingCircle(progress: number): Phaser.Geom.Circle {
-        const radius =
-            (1 - progress) * this.MAX_TIMING_RADIUS + this.HIT_RADIUS;
+    private getTimingCircle(): Phaser.Geom.Circle {
+        const radius = this.HIT_RADIUS + this.timingRadius;
         const timingCircle = new Phaser.Geom.Circle(0, 0, radius);
         return timingCircle;
     }
--- client/src/gameplay/scenes/playScene.ts
+++ client/src/gameplay/scenes/playScene.ts
@@ -7,18 +7,68 @@ export default class PlayScene extends Phaser.Scene {
     constructor() {
         super({ key: 'play' });
         this.gameElements = [];
+        window.debugObj = this;
     }
 
     public init(): void {
-        this.add.image(400, 300, 'sky');
+        const img = this.add.image(this.cameras.main.width/2, this.cameras.main.height/2, 'sky');
+        img.displayWidth = this.cameras.main.width;
+        img.displayHeight = this.cameras.main.width;
         this.sound.add('softHitclap');
     }
 
     public create(): void {
         this.createCircle(200, 200);
-        this.time.addEvent({
-            delay: 1000,
-            callback: () => this.createCircle(600, 400),
+        this.createCircle(600, 400);
+
+        const hitObjectTweens = [];
+
+        // for (let i = 0; i < 20; i++) {
+        //     window.beatmap.
+        // }
+        //
+        const timeline = this.tweens.timeline({
+            ease: 'linear',
+            duration: 5000,
+
+            tweens: [{
+                targets: this.gameElements[0],
+                onStart: this.gameElements[0].start,
+                onStartScope: this.gameElements[0],
+                alpha: 1,
+                offset: 1000,
+                duration: 500,
+            }, {
+                targets: this.gameElements[0],
+                timingRadius: 0,
+                offset: 1000,
+                duration: 1000,
+            }, {
+                targets: this.gameElements[0],
+                onEnd: this.gameElements[0].done,
+                onEndScope: this.gameElements[0],
+                alpha: 0,
+                offset: 2000,
+                duration: 100,
+            }, {
+                targets: this.gameElements[1],
+                onStart: this.gameElements[1].start,
+                onStartScope: this.gameElements[1],
+                alpha: 1,
+                offset: 2000,
+                duration: 500,
+            }, {
+                targets: this.gameElements[1],
+                timingRadius: 1,
+                offset: 2000,
+                duration: 1000,
+            }, {
+                targets: this.gameElements[1],
+                onEnd: this.gameElements[1].done,
+                onEndScope: this.gameElements[1],
+                alpha: 0,
+                duration: 100,
+            }]
         });
     }
 
@@ -30,6 +80,6 @@ export default class PlayScene extends Phaser.Scene {
 
     private createCircle(x: number, y: number): void {
         const hitCircle = new HitCircle(this, x, y);
-        this.gameElements.push(hitCircle);
+        this.gameElements.push(hitCircle)
     }
 }
